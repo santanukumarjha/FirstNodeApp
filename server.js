9224e7ee62b8c42a50c7f3d0d1846e88c5551dbe
Santanu Kumar <santanu.kumar@edgeverve.com>
2018-01-27 03:24:46 +0530

    Deprecated method removed from server file.

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ var routes = require('./api/routers/node-app-router'); //importing route
 routes(app); //register the route
 
 app.get('/display', function(req, res) {
-        res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname +'/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 app.get('*', function(req, res) {
-        res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
+         res.sendFile(__dirname +'/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 app.listen(port);
 
