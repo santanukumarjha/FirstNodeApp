4114c859a3aabf82f418ed9adcf46946280594c9
Santanu Kumar <santanu.kumar@edgeverve.com>
2018-01-28 10:35:20 +0530

    modified server file.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/Tododb'); 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
